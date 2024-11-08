a93e5866e785da0a08dc7b8a59827996312b986e
Alyssa Marche <alyssa.marche@student.ehb.be>
2024-11-08 09:38:19 +0100

    mouse event toegevoegd

--- milestone-2.js
+++ milestone-2.js
@@ -8,35 +8,32 @@ let h = context.canvas.height;
 
 draw();
 function draw() {
-context.fillRect(0, 0, w, h);
-drawNoiseCircle();
+	context.fillRect(0, 0, w, h);
+	drawNoiseCircle();
 }
 window.onmousemove = move;
 /**
 @param {mouseEvent} eventData
  */
 
-
 function move(eventData) {
 	let x = eventData.pageX;
 	let y = eventData.pageY;
-}
-
-
-function drawNoiseCircle() {
-
-	context.fillStyle = "black"
+		context.fillStyle = "black";
 	context.fillRect(0, 0, w, h);
-	for(let j = 0; j < 20; j++){
-        context.fillStyle = Utils.hsla(0, 0, Math.random() * 100, 50);
+	for (let j = 0; j < 20; j++) {
+		context.fillStyle = Utils.hsla(0, 0, Math.random() * 100, 50);
 		context.fillText("MCT", Math.random() * w, Math.random() * h);
 		context.font = "normal 60pt Arial";
 
-	context.fillStyle = Utils.hsl(j*36, 100, 50);
-	for (let i = 0; i < w; i++) {
-		let y = Noise.perlinNoise(i / 200);
-		Utils.fillCircle(i, y*h - 200 + j * 40, 5);
+		context.fillStyle = Utils.hsl(j * 36, 100, 50);
+		for (let i = 0; i < w; i++) {
+			let a = Noise.perlinNoise(i / 200);
+			Utils.fillCircle(i, a * h - x + j * y, 5);
+		}
 	}
 }
+
+function drawNoiseCircle() {
 
 }

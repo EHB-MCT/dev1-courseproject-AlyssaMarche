81db5d09dbd6da1ec07021a06d8d7f21b2986b15
Alyssa Marche <alyssa.marche@student.ehb.be>
2024-12-17 11:08:13 +0100

    changed to candy canes and trying to implement my space invador

--- spaceInvader.js
+++ spaceInvader.js
@@ -8,22 +8,22 @@ function drawSpaceInvador() {
 	canvas.height = window.innerHeight;
 	let context = canvas.getContext("2d");
 
-	context.fillStyle = "black";
-	context.fillRect(50, 50, 350, 350);
+	context.fillStyle = "green";
+	context.fillRect(850, 350, 350, 350);
 
 	context.fillStyle = "lightGreen";
 
 	context.beginPath();
-	context.fillRect(100, 100, 250, 250);
+	context.fillRect(900, 400, 250, 250);
 	context.fill();
 
-	context.fillStyle = "black";
+	context.fillStyle = "green";
 
-	context.fillRect(200, 100, 50, 50);
-	context.fillRect(200, 150, 50, 50);
-	context.fillRect(150, 150, 50, 50);
-	context.fillRect(250, 150, 50, 50);
-	context.fillRect(100, 200, 50, 50);
-	context.fillRect(300, 200, 50, 50);
-	context.fillRect(200, 250, 50, 50);
+	context.fillRect(1000, 400, 50, 50);
+	context.fillRect(1000, 550, 50, 50);
+	context.fillRect(1050, 450, 50, 50);
+	context.fillRect(1000, 450, 50, 50);
+	context.fillRect(950, 450, 50, 50);
+	context.fillRect(1100, 500, 50, 50);
+	context.fillRect(900, 500, 50, 50);
 }
